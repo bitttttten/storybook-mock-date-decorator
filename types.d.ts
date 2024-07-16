6992240d087a4f44b0c779ef9ce0098e9243535a
bitttttten <bitttttten@googlemail.com>
2024-07-17 00:37:23 +0200

    docs: add comments on types

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,11 @@
 import { MakeDecoratorResult } from "@storybook/addons";
 
+// This module is for Storybook v8
 declare module "storybook-mock-date-decorator" {
+  export const mockDateDecorator: MakeDecoratorResult;
+}
+
+// This module is for Storybook v6 and v7
+declare module "storybook-mock-date-decorator/legacy" {
   export const mockDateDecorator: MakeDecoratorResult;
 }
